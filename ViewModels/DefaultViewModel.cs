1d4e96acb2f7b6e0af21e5f1be51abd95e4faa8b
Daniel Gomez <daniel.gomez@studentpartner.com>
2020-05-21 18:55:36 -0500

    FC

---
+++ ViewModels/DefaultViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+using DotVVM.Framework.Hosting;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Builder;
+
+namespace DotVVMControls.ViewModels
+{
+    public class DefaultViewModel : MasterPageViewModel
+    {
+		public string Title { get; set;}
+		public PersonModel Person { get; set; } = new PersonModel { EnrollmentDate = DateTime.UtcNow.Date };
+		
+
+		public DefaultViewModel()
+		{
+			Title = "Person Form";
+		}
+
+		public void Process()
+        {
+			String script = "alert('" + "Welcome" + " " + Person.Username + " to Web App :) ')";
+			Context.ResourceManager.AddStartupScript(script);
+		}
+
+	}
+}
---
+++ ViewModels/PersonModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotVVMControls.ViewModels
+{
+    public class PersonModel
+    {
+		[Required]
+		public string Username { get; set; }
+		[Required]
+		public DateTime EnrollmentDate { get; set; }
+		[Required]
+		public string Gender { get; set; }
+		public string About { get; set; }
+	}
+}
